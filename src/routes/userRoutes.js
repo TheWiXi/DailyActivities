8900bf2f01006c536188a3998748eac1576e148a
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-10-23 20:29:58 -0500

    fix: :pencil2: se corrigen rutas de los endpoints segun la rubrica y se añade el ratelimiter

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middlewares/auth'); // Middleware de autenticación JWT
+const rateLimiter = require('../middlewares/rateLimiter'); // Middleware de límite de solicitudes
+
 
 // Rutas públicas
-router.post('/registro', userController.crear);
-router.post('/login', userController.iniciarSesion);
+router.post('/',rateLimiter.postLimiter, userController.crear);
+router.post('/iniciarSesion',rateLimiter.loginLimiter, userController.iniciarSesion);
 
 // Rutas protegidas (requieren autenticación)
-router.get('/validar-sesion', auth, userController.validarSesion);
-router.get('/', auth, userController.obtenerTodos);
-router.get('/:id', auth, userController.obtenerPorId);
-router.put('/:id', auth, userController.actualizar);
-router.delete('/:id', auth, userController.eliminar);
+router.get('/validarSesion', auth,rateLimiter.getLimiter, userController.validarSesion);
+router.get('/', auth,rateLimiter.getLimiter, userController.obtenerTodos);
+router.get('/:id', auth,rateLimiter.getLimiter, userController.obtenerPorId);
+router.put('/:id', auth,rateLimiter.putLimiter, userController.actualizar);
+router.delete('/:id', auth,rateLimiter.deleteLimiter, userController.eliminar);
 
 module.exports = router;
