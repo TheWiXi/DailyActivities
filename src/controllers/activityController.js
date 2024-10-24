14bc12f53bfcda0514b359f60353429b5966493b
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-10-24 15:22:28 -0500

    feat: :sparkles: se agrega funcion de obtener todas las actividades

--- src/controllers/activityController.js
+++ src/controllers/activityController.js
@@ -1,10 +1,13 @@
 const Activity = require('../models/Activity');
 const { formatResponse } = require('../utils/responseFormatter');
 const { validationResult } = require('express-validator');
+const Category = require('../models/Category');
+const Tag = require('../models/Tag');
+
 
 const activityController = {
   // Crear una nueva actividad
-  crear: async (req, res) => {
+  async crear(req, res) {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -19,12 +22,36 @@ const activityController = {
         const actividadGuardada = await newActivity.save();
         
         return res.status(201).json(formatResponse(201,"Actividad creada exitosamente",actividadGuardada));
-        
+
     } catch (error) {
         console.error('Error al crear actividad:', error);
         return res.status(500).json(formatResponse(500, 'Error al crear actividad'));
     }
-  } 
+  },
+  // Obtener todas las actividades del usuario
+  async obtenerTodas (req, res) {
+    try {
+        const newCategory = new Category();
+        const newtag = new Tag();
+        const actividades = await Activity.find({ usuario: req.user.id })
+            .populate('categoria', 'nombre color')
+            .populate('etiquetas', 'nombre color')
+            .populate('colaboradores', 'nombre apellido email')
+            .sort({ fecha_inicio: -1 });
+
+        return res.status(200).json({
+            status: 200,
+            message: "Actividades recuperadas exitosamente",
+            data: actividades
+        });
+    } catch (error) {
+      console.error('Error al obtener actividades:', error);
+      return res.status(500).json({
+          status: 500,
+          message: "Error interno del servidor"
+    });
+  }
+}
 };
 
 module.exports = activityController;
